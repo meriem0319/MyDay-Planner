997e7699d418d70dfd2ea21c565d97f286f9dcee
meriem0319 <meriem0319@gmail.com>
2022-07-05 10:40:15 -0400

    JS / HTML / CSS modification
    JS:
    -Added functions if/else for color coding the calendar
    -Added event listener for the save button and saved to localstorage
    
    HTML:
    -Added each time slot components so i can make my JS code after
    
    CSS mods:
    -combined certain components since i have them in the same class in my HTML
    -added a font weight to the hour so it stands out on the calendar
    -CSS wasn't modified too much, all was already given just needed to correspond the classes given into my HTML

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Jumbotron ONLY
 // adding the now moment info to diplay on the jumbotron
 var nowTimeElement = $("#now-moment");
 
@@ -7,3 +8,33 @@ function updateTime() {
     nowTimeElement.text(now);    
 }
 setInterval(updateTime, 1000);
+
+// Functions for the data in the calendar ONLY
+
+// decalring the only the HH value from moment.js so we can use it to compare
+var nowHour = moment().format("HH"); 
+
+$(".time-slot").each(function () {
+    var timeSlot = $(this).attr("id").split("-")[1];
+    //if the HH in nowHour is equal to the html id in its respective div then we add class present from our css
+    if (nowHour === timeSlot) {
+        $(this).addClass("present");
+    //if the HH in nowHour is less than the html id in its respective div then we add class future from our css
+    } else if (nowHour < timeSlot) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    //if the HH in nowHour is greater than the html id in its respective div then we add class present from our css
+    } else if (nowHour > timeSlot) {        
+        $(this).removeClass("future");
+        $(this).addClass("past");
+    }
+});
+
+// adding event listener to save users input in the text area to local storage
+$(".saveBtn").click(function (event) {
+    event.preventDefault(); //so it doesn't refresh
+    var textEl = $(this).siblings(".description").val();
+    var timeEl = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(timeEl, textEl);
+});
+
